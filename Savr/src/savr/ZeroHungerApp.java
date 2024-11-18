2a2e5acd8803e69584ae68cf7c0a1ce7bf46b6d6
Ryan Sheridan <0xryann@protonmail.com>
2024-11-18 10:16:54 +0000

    major bug fix in agri ui, sorry sean

--- Savr/src/savr/ZeroHungerApp.java
+++ Savr/src/savr/ZeroHungerApp.java
@@ -191,7 +191,7 @@ public class ZeroHungerApp extends javax.swing.JFrame {
             javax.swing.UIManager.put("Table.background", new Color(40, 40, 40));
             //javax.swing.UIManager.put("Button.background", new Color(60, 60, 60));
             //javax.swing.UIManager.put("Button.foreground", Color.WHITE);
-            //javax.swing.UIManager.put("TextField.background", new Color(40, 40, 40));
+            javax.swing.UIManager.put("TextField.background", new Color(40, 40, 40));
             //javax.swing.UIManager.put("TextField.foreground", Color.WHITE);
             //javax.swing.UIManager.put("TextField.caretForeground", Color.WHITE);
             javax.swing.UIManager.put("Label.foreground", Color.WHITE);
